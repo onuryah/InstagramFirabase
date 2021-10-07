7a58f5249b6cc6e34c8df32cfb11122577ec7ac5
Onuralp <xonuraralpx@gmail.com>
2021-10-07 21:46:16 +0300

    Initial Commit

---
+++ InstagramApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramApp
+//
+//  Created by Ceren Çapar on 7.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
