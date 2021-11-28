c09751c29e97c2e9de82b44eaeb58ace8898cca0
Onuralp <xonuraralpx@gmail.com>
2021-11-28 23:23:41 +0300

    first commit

---
+++ InstagramApp/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  InstagramApp
+//
+//  Created by Ceren Çapar on 12.10.2021.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
